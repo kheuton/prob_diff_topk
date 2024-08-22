51710afb9ee061d891074200bca90ca64deb893c
Kyle Heuton <kheuton@gmail.com>
2024-08-22 17:57:40 -0400

    no more separate samples, no more sum of nll

--- launch_torch_exps.py
+++ launch_torch_exps.py
@@ -2,45 +2,52 @@ import os
 import subprocess
 
 
-bpr_weights = [0, 5000, 100000]
-nll_weights = [0,1]
+bpr_weights = [1]
+nll_weights = [1]
 step_sizes = [0.1, 0.5, 0.001]
 perturbed_noises = [0.05, 0.01]
-initializations = ['bpr', 'nll']
-mus = [(10, 30), (10, 50), (30, 50)]
+init_indices = range(20)
+#mus = [(10, 30), (10, 50), (30, 50)]
 
 code_dir = '/cluster/home/kheuto01/code/prob_diff_topk'
-epochs=4000
+epochs=1000
 num_components=2
-seed=360
-thresholds=[0.99]
-base_dir = f'/cluster/tufts/hugheslab/kheuto01/synth_103050_datainit_{epochs}_comp{num_components}/'
+seeds=[360]
+
+base_dir = f'/cluster/tufts/hugheslab/kheuto01/didibreakitall_{epochs}_comp{num_components}/'
 
 count = 0
 for bpr_weight in bpr_weights:
     for nll_weight in nll_weights:
-        if bpr_weight == 0 and nll_weight == 0:
+        if (bpr_weight == 0 and nll_weight == 0):
             continue
         for step_size in step_sizes:
-            for threshold in thresholds:
-                for perturbed_noise in perturbed_noises:
-                    #for initialization in initializations:
-                        for mu1, mu2 in mus:
-                
-                            outdir = os.path.join(base_dir, f'mu1{mu1}mu2{mu2}_bw{bpr_weight}_nw{nll_weight}_sig{perturbed_noise}_ss{step_size}_th{threshold}')
+            #for threshold in thresholds:
+                for p, perturbed_noise in enumerate(perturbed_noises):
+                    for init_idx in init_indices:
+                        #for mu1, mu2 in mus:
+                        #for seed in seeds:
+                            outfolder = f'bw{bpr_weight}_nw{nll_weight}_ss{step_size}'
+                            if bpr_weight != 0:
+                                outfolder += f'_sig{perturbed_noise}'
+                            else:
+                                # bpr weight is 0, skip if this is second noise
+                                if p > 0:
+                                    continue
+                            outfolder += f'_init{init_idx}'
+
+                            outdir = os.path.join(base_dir, outfolder)
                             
                             arg_parts = [
                                         f"--epochs {epochs}",
                                         f"--step_size {step_size}",
                                         f"--bpr_weight {bpr_weight}",
                                         f"--nll_weight {nll_weight}",
-                                        f"--seed {seed}",
+                                        f"--init_idx {init_idx}",
                                         f"--outdir {outdir}",
-                                        f"--threshold {threshold}",
+                                        #f"--threshold {threshold}",
                                         f"--num_components {num_components}",
                                         f"--perturbed_noise {perturbed_noise}",
-                                        f"--mu1 {mu1}",
-                                        f"--mu2 {mu2}"
                                         ]
 
 
@@ -52,6 +59,7 @@ for bpr_weight in bpr_weights:
                             command = (f"code_dir={code_dir} args='{arg_cmd}' sbatch < /cluster/home/kheuto01/code/prob_diff_topk/run_exp.slurm")
                             subprocess.run(command, shell=True, check=True)
                             count += 1
+
 
 print(count)
             
--- torch_models.py
+++ torch_models.py
@@ -58,9 +58,101 @@ class MixtureOfPoissonsModel(nn.Module):
         # Create the MixtureSameFamily distribution
         mixture_dist = MixtureSameFamily(categorical_dist, poisson_dist,  validate_args=False)
         
-        
-        
         return mixture_dist
+
+class SpatialWaves(nn.Module):
+
+    def __init__(self, num_waves=2, min_peak_width=0.1, death_noise=0.2, low=0, high=100):
+        super(SpatialWaves, self).__init__()
+        self.low = low
+        self.high = high
+        self.num_waves = num_waves
+        self.arrival_intercepts =nn.Parameter(torch.rand(num_waves), requires_grad=True)
+        self.lat_coeff = nn.Parameter(torch.rand(num_waves), requires_grad=True)
+        self.lon_coeff = nn.Parameter(torch.rand(num_waves), requires_grad=True)
+        self.softplusinv_magnitudes = nn.Parameter(torch.rand(num_waves), requires_grad=True)
+        self.softplusinv_peak_widths = nn.Parameter(torch.rand(num_waves), requires_grad=True)
+        self.min_peak_width=min_peak_width
+        self.death_noise = death_noise
+
+    def params_to_single_tensor(self):
+        return torch.cat([param.view(-1) for param in self.parameters()])
+
+    def single_tensor_to_params(self, single_tensor):
+        arrival_intercepts = single_tensor[:self.num_waves]
+        lat_coeff = single_tensor[self.num_waves:2*self.num_waves]
+        lon_coeff = single_tensor[2*self.num_waves:3*self.num_waves]
+        softplusinv_magnitudes = single_tensor[3*self.num_waves:4*self.num_waves]
+        softplusinv_peak_widths = single_tensor[4*self.num_waves:5*self.num_waves]
+        return arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths
+    
+    def update_params(self, single_tensor):
+        arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths = self.single_tensor_to_params(single_tensor)
+        self.arrival_intercepts = arrival_intercepts
+        self.lat_coeff = lat_coeff
+        self.lon_coeff = lon_coeff
+        self.softplusinv_magnitudes = softplusinv_magnitudes
+        self.softplusinv_peak_widths = softplusinv_peak_widths
+
+        return
+    
+    def build_from_single_tensor(self, single_tensor, time_T, pop_S, lat_S, lon_S):
+
+        S = pop_S.shape[0]
+        T = time_T.shape[0]
+        W = self.num_waves
+
+        arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths = self.single_tensor_to_params(single_tensor)
+        magnitudes_W = torch.nn.functional.softplus(softplusinv_magnitudes)
+        peak_widths_W = torch.nn.functional.softplus(softplusinv_peak_widths) + self.min_peak_width
+        arrival_times_SW = arrival_intercepts.expand(S,W) +lat_S.unsqueeze(-1)*lat_coeff.expand(1,W) + lon_S.unsqueeze(-1)*lon_coeff.expand(1,W)
+        
+        # exponentiate time - arrival time / peak width
+        time_diff_TSW = time_T.unsqueeze(-1).unsqueeze(-1) - arrival_times_SW.expand(1,S,W)
+        # square time_diff/peak_width
+        waves_TSW = magnitudes_W.expand(1,1,W)*torch.exp(-(time_diff_TSW/peak_widths_W.expand(1,1,W))**2)
+        death_rate_TS = torch.sum(waves_TSW, dim=-1)
+        mean_deaths_TS = pop_S.expand(1,S)*death_rate_TS
+        return TruncatedNormal(mean_deaths_TS, self.death_noise, self.low, self.high, validate_args=False)
+    
+    def forward(self, time_T, pop_S, lat_S, lon_S):
+
+        S = pop_S.shape[0]
+        T = time_T.shape[0]
+        W = self.num_waves
+
+        magnitudes_W = torch.nn.functional.softplus(self.softplusinv_magnitudes)
+        peak_widths_W = torch.nn.functional.softplus(self.softplusinv_peak_widths) + self.min_peak_width
+
+
+        arrival_times_SW = self.arrival_intercepts.expand(S,W) +lat_S.unsqueeze(-1)*self.lat_coeff.expand(1,W) + lon_S.unsqueeze(-1)*self.lon_coeff.expand(1,W)
+        
+        # exponentiate time - arrival time / peak width
+        time_diff_TSW = time_T.unsqueeze(-1).unsqueeze(-1) - arrival_times_SW.expand(1,S,W)
+        # square time_diff/peak_width
+        waves_TSW = magnitudes_W.expand(1,1,W)*torch.exp(-(time_diff_TSW/peak_widths_W.expand(1,1,W))**2)
+        death_rate_TS = torch.sum(waves_TSW, dim=-1)
+        mean_deaths_TS = pop_S.expand(1,S)*death_rate_TS
+        return TruncatedNormal(mean_deaths_TS, self.death_noise, self.low, self.high, validate_args=False)
+
+    def plot_learned(self,time, pop, lat, lon, data=None, ax=None):
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import seaborn as sns
+        
+        dist = self.forward(time, pop, lat, lon)
+
+        samples = dist.sample((1000,))
+        samples = samples.view(-1).detach().numpy()
+        # plot the kde only
+        sns.kdeplot(samples, bw_adjust=0.5, ax=ax)
+        # plot hist of data if present
+        if data is not None:
+            sns.histplot(data.flatten(), stat='density', bins=50, color='red', alpha=0.5, ax=ax)
+        plt.show()
+        
+        return
+
     
 class MixtureOfTruncNormModel(nn.Module):
     def __init__(self, num_components=4, S=12, low=0, high=200):
@@ -71,7 +163,7 @@ class MixtureOfTruncNormModel(nn.Module):
         self.high=high
         
         # Initialize the log rates and mixture probabilities as learnable parameters
-        self.softplusinv_means = nn.Parameter(torch.rand(num_components)*20, requires_grad=True) 
+        self.softplusinv_means = nn.Parameter(torch.rand(num_components)*50, requires_grad=True) 
         self.softplusinv_scales = nn.Parameter(torch.rand(num_components), requires_grad=True) 
         self.mixture_probs = nn.Parameter(torch.rand(S, num_components), requires_grad=True)  
 
