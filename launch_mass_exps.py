Gradient fix

launch_mass_exps.py
@@ -1,25 +1,27 @@
 import os
 import subprocess
+import numpy as np
 
 # Hyperparameter configurations
 Ks = [100]  # Number of top locations to consider
 bpr_weights = [0, 30]  # Weights for BPR loss term
 nll_weights = [0, 1]  # Weights for negative log-likelihood term
-step_sizes = [0.0001]  # Learning rates
-perturbed_noises = [0.01]  # Noise levels for perturbed top-k
-thresholds = [1]  # BPR thresholds
-score_sample_sizes = [ 50]  # Number of samples for score estimation
-pert_sample_sizes = [ 50]  # Number of samples for perturbation
+step_sizes = np.logspace(-5, -1, 5)  # Learning rates
+perturbed_noises = np.logspace(-5, -1, 5)  # Noise levels for perturbation
+thresholds = [1, 0.6, 0.65, 0.7, 0.5]  # BPR thresholds
+score_sample_sizes = [100]  # Number of samples for score estimation
+pert_sample_sizes = [100]  # Number of samples for perturbation
 
 # Fixed parameters
 epochs = 8000
-seeds = [360]  # Multiple seeds for reproducibility
+seeds = [123]  # Multiple seeds for reproducibility
 code_dir = '/cluster/home/kheuto01/code/prob_diff_topk'
+data_dir = '/cluster/tufts/hugheslab/datasets/NSF_OD/cleaned/MA'
 
 count = 0
 for K in Ks:
     # Create base directory for this K value
-    base_dir = f'/cluster/tufts/hugheslab/kheuto01/bird_zero_rand_K{K}_{epochs}/'
+    base_dir = f'/cluster/tufts/hugheslab/kheuto01/opioid_grid_fix_grad/MA/'
     
     for bpr_weight in bpr_weights:
         for nll_weight in nll_weights:
@@ -65,7 +67,7 @@ for K in Ks:
                                         f"--num_score_samples {num_score_samples}",
                                         f"--num_pert_samples {num_pert_samples}",
                                         f"--perturbed_noise {perturbed_noise}",
-                                        f"--data_dir {code_dir}",
+                                        f"--data_dir {data_dir}",
                                         "--device cuda"
                                     ]
 
@@ -78,10 +80,7 @@ for K in Ks:
                                         
                                     # Launch the job using slurm
                                     command = f"code_dir={code_dir} args='{arg_cmd}' sbatch < /cluster/home/kheuto01/code/prob_diff_topk/run_neg_binom.slurm"
-                                    #subprocess.run(command, shell=True, check=True)
-                                    if count<1:
-                                        print(command)
-                                    break
+                                    subprocess.run(command, shell=True, check=True)
                                     count += 1
 
                                     

torch_models.py
@@ -12,14 +12,14 @@ class NegativeBinomialRegressionModel(nn.Module):
         self.num_fixed_effects = num_fixed_effects
         self.low = low
         self.high = high
-
+        
         # Fixed effects
         self.beta_0 = nn.Parameter(torch.randn(1))
         self.beta = nn.Parameter(torch.randn(num_fixed_effects))
 
-        # Random effects
-        self.b_0 = torch.zeros(num_locations).to(device)
-        self.b_1 = torch.zeros(num_locations).to(device)
+        # Random effects - why is this not param?
+        self.b_0 = nn.Parameter(torch.zeros(num_locations).to(device))
+        self.b_1 = nn.Parameter(torch.zeros(num_locations).to(device))
 
         # Covariance matrix parameters
         self.log_sigma_0 = nn.Parameter(torch.randn(1))
@@ -133,6 +133,25 @@ class NegativeBinomialRegressionModel(nn.Module):
         plt.show()
         
         return ax
+    
+class NegativeBinomialDebug(NegativeBinomialRegressionModel):
+
+    def build_from_single_tensor(self, single_tensor, X, time):
+        beta_0, beta, b_0, b_1, log_sigma_0, log_sigma_1, rho, softinv_theta = self.single_tensor_to_params(single_tensor)
+        fixed_effects = beta_0 + torch.einsum('tli,i->tl', X, beta)
+        random_intercepts = b_0.expand(X.shape[0], -1)
+        random_slopes = b_1.expand(X.shape[0], -1)
+        
+        log_mu = fixed_effects + random_intercepts + random_slopes * time
+
+        # Use softplus to ensure mu is positive and grows more slowly
+        mu = nn.functional.softplus(log_mu)
+
+        # Calculate theta probability
+        theta = torch.nn.functional.softplus(softinv_theta)
+
+        
+        return NegativeBinomial(total_count=mu, probs=theta)
 
 class MixtureOfPoissonsModel(nn.Module):
     def __init__(self, num_components=4, S=12):

torch_opioid_exp.py
@@ -8,7 +8,7 @@ import argparse
 from functools import partial
 from metrics import top_k_onehot_indicator
 from torch_perturb.perturbations import perturbed
-from torch_models import NegativeBinomialRegressionModel, torch_bpr_uncurried, deterministic_bpr
+from torch_models import NegativeBinomialDebug, torch_bpr_uncurried, deterministic_bpr
 
 
 def convert_df_to_3d_array(df):
@@ -166,12 +166,12 @@ def main(K=None, step_size=None, epochs=None, bpr_weight=None,
         np.random.seed(seed)
 
     # Load training data
-    train_X_df = pd.read_csv(os.path.join(data_dir, 'bird_train_x.csv'), index_col=[0,1])
-    train_Y_df = pd.read_csv(os.path.join(data_dir, 'bird_train_y.csv'), index_col=[0,1])
+    train_X_df = pd.read_csv(os.path.join(data_dir, 'train_x.csv'), index_col=[0,1])
+    train_Y_df = pd.read_csv(os.path.join(data_dir, 'train_y.csv'), index_col=[0,1])
     
     # Load validation data
-    val_X_df = pd.read_csv(os.path.join(data_dir, 'bird_valid_x.csv'), index_col=[0,1])
-    val_Y_df = pd.read_csv(os.path.join(data_dir, 'bird_valid_y.csv'), index_col=[0,1])
+    val_X_df = pd.read_csv(os.path.join(data_dir, 'valid_x.csv'), index_col=[0,1])
+    val_Y_df = pd.read_csv(os.path.join(data_dir, 'valid_y.csv'), index_col=[0,1])
     
     # Process training data
     train_X, geoids, timesteps = convert_df_to_3d_array(train_X_df)#.drop(columns='timestep.1'))
@@ -193,7 +193,7 @@ def main(K=None, step_size=None, epochs=None, bpr_weight=None,
     time_val = torch.tensor(val_time_arr, dtype=torch.float32).to(device)
 
     # Initialize model
-    model = NegativeBinomialRegressionModel(
+    model = NegativeBinomialDebug(
         num_locations=len(geoids),
         num_fixed_effects=train_X.shape[2], device=device
     ).to(device)