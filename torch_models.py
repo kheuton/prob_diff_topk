e6a8dc719bbb9c355c8a6752f5f3bb38d939152f
Kyle Heuton <kheuton@gmail.com>
2025-02-11 11:37:11 -0500

    Add cleaned datasets, add hyperparameter search

--- torch_models.py
+++ torch_models.py
@@ -109,8 +109,21 @@ class NegativeBinomialRegressionModel(nn.Module):
             param.data = new_data
 
     def build_from_single_tensor(self, single_tensor, X, time):
-        self.update_params(single_tensor)
-        return self.forward(X, time)
+        beta_0, beta, b_0, b_1, log_sigma_0, log_sigma_1, rho, siginv_theta = self.single_tensor_to_params(single_tensor)
+        fixed_effects = beta_0 + torch.einsum('tli,i->tl', X, beta)
+        random_intercepts = b_0.expand(X.shape[0], -1)
+        random_slopes = b_1.expand(X.shape[0], -1)
+        
+        log_mu = fixed_effects + random_intercepts + random_slopes * time
+
+        # Use softplus to ensure mu is positive and grows more slowly
+        mu = nn.functional.softplus(log_mu)
+
+        # Calculate theta probability
+        theta = torch.nn.functional.sigmoid(siginv_theta)
+
+        
+        return NegativeBinomial(total_count=mu, probs=theta)
 
     def plot_learned(self, X, time, data=None, ax=None):
         import matplotlib.pyplot as plt
@@ -132,25 +145,6 @@ class NegativeBinomialRegressionModel(nn.Module):
         
         return ax
     
-class NegativeBinomialDebug(NegativeBinomialRegressionModel):
-
-    def build_from_single_tensor(self, single_tensor, X, time):
-        beta_0, beta, b_0, b_1, log_sigma_0, log_sigma_1, rho, siginv_theta = self.single_tensor_to_params(single_tensor)
-        fixed_effects = beta_0 + torch.einsum('tli,i->tl', X, beta)
-        random_intercepts = b_0.expand(X.shape[0], -1)
-        random_slopes = b_1.expand(X.shape[0], -1)
-        
-        log_mu = fixed_effects + random_intercepts + random_slopes * time
-
-        # Use softplus to ensure mu is positive and grows more slowly
-        mu = nn.functional.softplus(log_mu)
-
-        # Calculate theta probability
-        theta = torch.nn.functional.sigmoid(siginv_theta)
-
-        
-        return NegativeBinomial(total_count=mu, probs=theta)
-
 class MixtureOfPoissonsModel(nn.Module):
     def __init__(self, num_components=4, S=12):
         super(MixtureOfPoissonsModel, self).__init__()
@@ -206,124 +200,6 @@ class MixtureOfPoissonsModel(nn.Module):
         
         return mixture_dist
 
-class SpatialWaves(nn.Module):
-
-    def __init__(self, num_waves=2, min_peak_width=0.1, death_noise=0.2, low=0, high=100, has_speed=False):
-        super(SpatialWaves, self).__init__()
-        self.low = low
-        self.high = high
-        self.num_waves = num_waves
-        self.has_speed = has_speed
-        if self.has_speed:
-            self.arrival_speeds =nn.Parameter(torch.rand(num_waves), requires_grad=True)
-        self.arrival_intercepts =nn.Parameter(torch.rand(num_waves)+12, requires_grad=True)
-        self.lat_coeff = nn.Parameter(-torch.rand(num_waves), requires_grad=True)
-        self.lon_coeff = nn.Parameter(-torch.rand(num_waves), requires_grad=True)
-        # magnitudes are random between -6 and -5
-        self.softplusinv_magnitudes = nn.Parameter(torch.rand(num_waves)-6.0, requires_grad=True)
-        self.softplusinv_peak_widths = nn.Parameter(torch.rand(num_waves)+2.0, requires_grad=True)
-        self.min_peak_width=min_peak_width
-        self.death_noise = death_noise
-
-    def params_to_single_tensor(self):
-        return torch.cat([param.view(-1) for param in self.parameters()])
-
-    def single_tensor_to_params(self, single_tensor):
-        if self.has_speed:
-            arrival_speeds = single_tensor[:self.num_waves]
-            speed_offset = 1
-        else:
-            speed_offset = 0
-        arrival_intercepts = single_tensor[self.num_waves*(speed_offset):(1+speed_offset)*self.num_waves]
-        lat_coeff = single_tensor[(1+speed_offset)*self.num_waves:(2+speed_offset)*self.num_waves]
-        lon_coeff = single_tensor[(2+speed_offset)*self.num_waves:(3+speed_offset)*self.num_waves]
-        softplusinv_magnitudes = single_tensor[(3+speed_offset)*self.num_waves:(4+speed_offset)*self.num_waves]
-        softplusinv_peak_widths = single_tensor[(4+speed_offset)*self.num_waves:(5+speed_offset)*self.num_waves]
-        if self.has_speed:
-            return arrival_speeds, arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths
-        else:
-            return arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths
-    
-    def update_params(self, single_tensor):
-        if self.has_speed:
-            arrival_speeds, arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths = self.single_tensor_to_params(single_tensor)
-            self.arrival_speeds.data = arrival_speeds
-        else:
-            arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths = self.single_tensor_to_params(single_tensor)
-        self.arrival_intercepts.data = arrival_intercepts
-        self.lat_coeff.data = lat_coeff
-        self.lon_coeff.data = lon_coeff
-        self.softplusinv_magnitudes.data = softplusinv_magnitudes
-        self.softplusinv_peak_widths.data = softplusinv_peak_widths
-
-        return
-    
-    def build_from_single_tensor(self, single_tensor, time_T, pop_S, lat_S, lon_S):
-
-        S = pop_S.shape[0]
-        T = time_T.shape[0]
-        W = self.num_waves
-        if self.has_speed:
-            arrival_speeds, arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths = self.single_tensor_to_params(single_tensor)
-        else:
-            arrival_intercepts, lat_coeff, lon_coeff, softplusinv_magnitudes, softplusinv_peak_widths = self.single_tensor_to_params(single_tensor)
-        
-        magnitudes_W = torch.nn.functional.softplus(softplusinv_magnitudes)
-        peak_widths_W = torch.nn.functional.softplus(softplusinv_peak_widths) + self.min_peak_width
-        arrival_times_SW = arrival_intercepts.expand(S,W) +lat_S.unsqueeze(-1)*lat_coeff.expand(1,W) + lon_S.unsqueeze(-1)*lon_coeff.expand(1,W)
-        
-        # exponentiate time - arrival time / peak width
-        if self.has_speed:
-            time_diff_TSW = arrival_speeds.expand(1,1,W)*time_T.unsqueeze(-1).unsqueeze(-1) - arrival_times_SW.expand(1,S,W)
-        else:
-            time_diff_TSW = time_T.unsqueeze(-1).unsqueeze(-1) - arrival_times_SW.expand(1,S,W)
-        # square time_diff/peak_width
-        waves_TSW = magnitudes_W.expand(1,1,W)*torch.exp(-(time_diff_TSW/peak_widths_W.expand(1,1,W))**2)
-        death_rate_TS = torch.sum(waves_TSW, dim=-1)
-        mean_deaths_TS = pop_S.expand(1,S)*death_rate_TS
-        return TruncatedNormal(mean_deaths_TS, self.death_noise, self.low, self.high, validate_args=False)
-    
-    def forward(self, time_T, pop_S, lat_S, lon_S):
-
-        S = pop_S.shape[0]
-        T = time_T.shape[0]
-        W = self.num_waves
-
-        magnitudes_W = torch.nn.functional.softplus(self.softplusinv_magnitudes)
-        peak_widths_W = torch.nn.functional.softplus(self.softplusinv_peak_widths) + self.min_peak_width
-
-
-        arrival_times_SW = self.arrival_intercepts.expand(S,W) +lat_S.unsqueeze(-1)*self.lat_coeff.expand(1,W) + lon_S.unsqueeze(-1)*self.lon_coeff.expand(1,W)
-        
-        # exponentiate time - arrival time / peak width
-        if self.has_speed:
-            time_diff_TSW = self.arrival_speeds.expand(1,1,W)*time_T.unsqueeze(-1).unsqueeze(-1) - arrival_times_SW.expand(1,S,W)
-        else:
-            time_diff_TSW = time_T.unsqueeze(-1).unsqueeze(-1) - arrival_times_SW.expand(1,S,W)
-            print(time_diff_TSW.shape)
-        # square time_diff/peak_width
-        waves_TSW = magnitudes_W.expand(1,1,W)*torch.exp(-(time_diff_TSW/peak_widths_W.expand(1,1,W))**2)
-        death_rate_TS = torch.sum(waves_TSW, dim=-1)
-        mean_deaths_TS = pop_S.expand(1,S)*death_rate_TS
-        return TruncatedNormal(mean_deaths_TS, self.death_noise, self.low, self.high, validate_args=False)
-
-    def plot_learned(self,time, pop, lat, lon, data=None, ax=None):
-        import matplotlib.pyplot as plt
-        import numpy as np
-        import seaborn as sns
-        
-        dist = self.forward(time, pop, lat, lon)
-
-        samples = dist.sample((1000,))
-        samples = samples.view(-1).detach().numpy()
-        # plot the kde only
-        sns.kdeplot(samples, bw_adjust=0.5, ax=ax)
-        # plot hist of data if present
-        if data is not None:
-            sns.histplot(data.flatten(), stat='density', bins=50, color='red', alpha=0.5, ax=ax)
-        plt.show()
-        
-        return
 
     
 class MixtureOfTruncNormModel(nn.Module):
